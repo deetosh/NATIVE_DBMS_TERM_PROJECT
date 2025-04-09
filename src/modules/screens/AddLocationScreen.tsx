38279713f795ab757c4d6010462bca56936cf69a
deetosh <deetoshkuila18@gmail.com>
2025-04-09 20:31:51 +0530

    merged deetosh-add-location and bus details screen

--- src/modules/screens/AddLocationScreen.tsx
+++ src/modules/screens/AddLocationScreen.tsx
@@ -1,12 +1,293 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  Dimensions,
+  Alert,
+} from 'react-native';
+import MapView, {Marker, MapPressEvent, Region} from 'react-native-maps';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import Icon from 'react-native-vector-icons/Ionicons';
+import {COLOR, initialRegionMap} from '../../constants';
+import Toast from 'react-native-toast-message';
+import { callAPI } from '../../services/callApi';
+import Loader from '../../molecules/Loader';
+
+const {width} = Dimensions.get('window');
+
+interface Coordinate {
+  latitude: number;
+  longitude: number;
+}
+
+interface Location {
+  name: string;
+  coordinates: number[];
+}
 
 const AddLocationScreen: React.FC = () => {
+  const [isAdding, setIsAdding] = useState<boolean>(false);
+  const [placeName, setPlaceName] = useState<string>('');
+  const [selectedCoordinate, setSelectedCoordinate] =
+    useState<Coordinate | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [allLocations, setAllLocations] = useState<Location[]>([]);
+
+  const handleMapPress = (e: MapPressEvent) => {
+    if (isAdding) {
+      const coordinate = e.nativeEvent.coordinate;
+      setSelectedCoordinate(coordinate);
+    }
+  };
+
+  const handleSubmit = async () => {
+    if (!placeName || !selectedCoordinate) {
+      // Alert.alert(
+      //   'Missing Info',
+      //   'Please enter a place name and tap on the map.',
+      // );
+      Toast.show({
+        type: 'error',
+        text1: 'Missing Info',
+        text2: 'Please enter a place name and tap on the map to select a place.',
+      });
+      return;
+    }
+
+    try {
+      const response = await callAPI('/location/add',"POST",{
+        name: placeName,
+        coordinates: [selectedCoordinate.latitude,selectedCoordinate.longitude],
+      });
+
+      if (!response.isError) {
+        Toast.show({
+          type: 'success',
+          text1: 'Success',
+          text2: 'Location added successfully.',
+        });
+        setAllLocations((prevLocations) => [
+          ...prevLocations,
+          {
+            name: placeName,
+            coordinates: [
+              selectedCoordinate.latitude,
+              selectedCoordinate.longitude,
+            ],
+          },
+        ]);
+        setPlaceName('');
+        setSelectedCoordinate(null);
+        setIsAdding(false);
+      } else {
+        Toast.show({
+          type: 'error',
+          text1: 'Error',
+          text2: 'Failed to add location. Please try again.',
+        });
+        setIsAdding(false);
+        setPlaceName('');
+        setSelectedCoordinate(null);
+      }
+    } catch (error) {
+      Toast.show({
+        type: 'error',
+        text1: 'Error',
+        text2: 'Failed to add location. Please try again.',
+      });
+      setIsAdding(false);
+      setPlaceName('');
+      setSelectedCoordinate(null);
+    }
+  };
+
+  const handleCancel = () => {
+    setIsAdding(false);
+    setPlaceName('');
+    setSelectedCoordinate(null);
+  };
+
+  useEffect(()=> {
+    const fetchLocations = async () => {
+      try {
+        setIsLoading(true);
+        const response = await callAPI('/location/get', "GET");
+        if (!response.isError) {
+          setAllLocations(response.data);
+        } else {
+          Toast.show({
+            type: 'error',
+            text1: 'Error',
+            text2: 'Failed to fetch locations. Please try again.',
+          });
+        }
+        setIsLoading(false);
+      } catch (error) {
+        setIsLoading(false);
+        Toast.show({
+          type: 'error',
+          text1: 'Error',
+          text2: 'Failed to fetch locations. Please try again.',
+        });
+      }
+    };
+    fetchLocations();
+  },[])
+
+  const initialRegion: Region = initialRegionMap;
+
   return (
-    <View>
-      <Text>Home Screen</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Loader visible={isLoading} />
+      <View style={styles.container}>
+        <MapView
+          style={styles.map}
+          showsUserLocation={true}
+          initialRegion={initialRegion}
+          onPress={handleMapPress}>
+          {selectedCoordinate && <Marker coordinate={selectedCoordinate} />}
+          {allLocations.map((location, index) => (
+            <Marker
+              key={index}
+              coordinate={{
+                latitude: location.coordinates[0],
+                longitude: location.coordinates[1],
+              }}
+              title={location.name}
+              pinColor='blue'
+            />
+          ))}
+        </MapView>
+
+        {!isAdding && (
+          <TouchableOpacity
+            style={styles.plusButton}
+            onPress={() => setIsAdding(true)}>
+            <Icon name="add" size={30} color="#fff" />
+          </TouchableOpacity>
+        )}
+
+        {isAdding && (
+          <View style={styles.inputContainer}>
+            <TextInput
+              style={styles.textInput}
+              placeholder="Enter place name"
+              value={placeName}
+              onChangeText={setPlaceName}
+              placeholderTextColor="gray"
+              autoFocus={true}
+            />
+            <TextInput
+              style={styles.disabledtextInput}
+              placeholder="Select a point on map (Latitude)"
+              value={
+                selectedCoordinate?.latitude
+                  ? `${selectedCoordinate.latitude}`
+                  : ''
+              }
+              editable={false}
+              selectTextOnFocus={false}
+              placeholderTextColor="gray"
+            />
+            <TextInput
+              style={styles.disabledtextInput}
+              placeholder="Select a point on map (Longitude)"
+              value={
+                selectedCoordinate?.longitude
+                  ? `${selectedCoordinate.longitude}`
+                  : ''
+              }
+              editable={false}
+              selectTextOnFocus={false}
+              placeholderTextColor="gray"
+            />
+            <View
+              style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+              <TouchableOpacity
+                style={styles.cancelButton}
+                onPress={handleCancel}>
+                <Text style={styles.submitText}>Cancel</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={styles.submitButton}
+                onPress={handleSubmit}>
+                <Text style={styles.submitText}>Submit</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        )}
+      </View>
+    </SafeAreaView>
   );
 };
 
 export default AddLocationScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingBottom: 30,
+  },
+  map: {
+    flex: 1,
+  },
+  plusButton: {
+    position: 'absolute',
+    bottom: 40,
+    right: 30,
+    backgroundColor: COLOR.bg_primary,
+    borderRadius: 30,
+    width: 60,
+    height: 60,
+    padding: 15,
+    elevation: 2,
+  },
+  inputContainer: {
+    position: 'absolute',
+    // bottom: 0,
+    top: 0,
+    width: '100%',
+    padding: 20,
+    backgroundColor: COLOR.bg_primary,
+  },
+  textInput: {
+    height: 45,
+    borderColor: COLOR.text_secondary,
+    borderWidth: 1,
+    paddingHorizontal: 10,
+    borderRadius: 5,
+    marginBottom: 10,
+    color: COLOR.text_primary,
+    fontSize: 16,
+  },
+  disabledtextInput: {
+    height: 45,
+    borderColor: COLOR.text_secondary,
+    backgroundColor: COLOR.bg_secondary,
+    borderWidth: 1,
+    paddingHorizontal: 10,
+    borderRadius: 5,
+    marginBottom: 10,
+    color: COLOR.text_primary,
+    fontSize: 16,
+  },
+  submitButton: {
+    backgroundColor: COLOR.btn_primary,
+    padding: 10,
+    alignItems: 'center',
+    borderRadius: 5,
+  },
+  submitText: {
+    color: COLOR.text_dark,
+    fontWeight: 'bold',
+  },
+  cancelButton: {
+    backgroundColor: COLOR.btn_secondary,
+    padding: 10,
+    alignItems: 'center',
+    borderRadius: 5,
+  },
+});
--- src/modules/screens/BusDetailsScreen.tsx
+++ src/modules/screens/BusDetailsScreen.tsx
@@ -1,12 +1,137 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+// components/BusDetails.tsx
+import React, {useEffect, useState} from 'react';
+import {View, Text, ActivityIndicator, StyleSheet} from 'react-native';
+import {callAPI} from '../../services/callApi'; // replace with your actual API helper
+import Loader from '../../molecules/Loader';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import { Picker } from "@react-native-picker/picker";
+import {COLOR} from '../../constants';
 
-const BusDetailsScreen: React.FC = () => {
+const BusDetailsScreen = () => {
+  const [busData, setBusData] = useState<any[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [selectedBusId, setSelectedBusId] = useState<string | null>(null); // Replace with actual bus ID fetching logic
+
+  const currentHour = new Date().getHours();
+
+  // useEffect(() => {
+  //   const fetchBus = async () => {
+  //     try {
+  //       const response = await callAPI(
+  //         `/bus/getById`,
+  //         'GET',
+  //         {},
+  //         {busId: busId},
+  //       );
+  //       setBusData(response.data);
+  //     } catch (err) {
+  //       console.error('Failed to fetch bus details', err);
+  //     } finally {
+  //       setLoading(false);
+  //     }
+  //   };
+  //   if (busId) fetchBus();
+  // }, [busId]);
+
+  useEffect(() => {
+    const fetchAllBuses = async () => {
+      try {
+        setLoading(true);
+        const response = await callAPI(`/bus/get`, 'GET', {}, {});
+        if(!response.isError){
+          console.log('Bus data:', response.data);
+          setBusData(response.data);
+        }
+      } catch (err) {
+        console.error('Failed to fetch bus details', err);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchAllBuses();
+  }, []);
+
   return (
-    <View>
-      <Text>Bus Details Screen</Text>
+    <View style={styles.container}>
+      {/* <Text style={styles.title}>Bus Details</Text>
+      <Text style={styles.info}>Bus Number: {busData.bus_number}</Text>
+      <Text style={styles.info}>Driver: {busData.driver?.username}</Text>
+
+      {busData.stoppage.map((stop: any, index: number) => (
+        <View key={index} style={styles.stopItem}>
+          <Text style={styles.stopName}>Location: {stop.location.name}</Text>
+          <Text style={styles.stopTime}>
+            Arrival Time: {currentHour}:{stop.time.toString().padStart(2, '0')}
+          </Text>
+        </View>
+      ))} */}
+      <View style={styles.pickerContainer}>
+        <Ionicons
+          name="bus-outline"
+          size={20}
+          color="white"
+          style={styles.icon}
+        />
+        <Picker
+          selectedValue={selectedBusId}
+          onValueChange={itemValue => setSelectedBusId(itemValue)}
+          mode="dropdown"
+          style={styles.picker}
+          dropdownIconColor={COLOR.text_primary}>
+          <Picker.Item
+            label="Select Bus"
+            value=""
+            color="grey"
+            style={styles.pickerLabel}
+          />
+          {busData.map((bus:any) => (
+            <Picker.Item
+              key={bus._id}
+              label={bus.bus_number}
+              value={bus._id}
+              style={styles.pickerLabel}
+            />
+          ))}
+        </Picker>
+      </View>
+      <Loader visible={loading} />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {backgroundColor: COLOR.bg_primary, padding: 16, flex: 1},
+  title: {fontSize: 18, fontWeight: 'bold', marginBottom: 12},
+  info: {fontSize: 16, marginBottom: 8},
+  stopItem: {
+    marginVertical: 8,
+    borderBottomColor: '#ccc',
+    borderBottomWidth: 1,
+    paddingBottom: 8,
+  },
+  stopName: {fontSize: 16, fontWeight: '600'},
+  stopTime: {fontSize: 14, color: 'gray'},
+  errorText: {padding: 20, color: 'red'},
+  picker: {
+    flex: 1,
+    color: COLOR.text_secondary,
+    fontSize: 18,
+  },
+  pickerLabel: {
+    backgroundColor: COLOR.bg_secondary,
+    color: COLOR.text_secondary,
+  },
+  pickerContainer: {
+    borderWidth: 1,
+    borderRadius: 8,
+    marginBottom: 10,
+    paddingHorizontal: 10,
+    borderColor: COLOR.bg_tertiary,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  icon: { marginRight: 10 },
+});
 
 export default BusDetailsScreen;
